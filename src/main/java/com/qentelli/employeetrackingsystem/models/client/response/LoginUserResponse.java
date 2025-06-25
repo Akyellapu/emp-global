8393c75eb0a4cc5fd99e206c54432ca59303ee88
Yellapu Anil Kumar <anil.yellapu@qentelli.com>
2025-06-25 16:29:07 +0530

    Initial commit

---
+++ src/main/java/com/qentelli/employeetrackingsystem/models/client/response/LoginUserResponse.java
@@ -0,0 +1,18 @@
+package com.qentelli.employeetrackingsystem.models.client.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginUserResponse {
+
+	private String userName;
+//	private String password;
+	private String role;
+	private String acessToken;
+
+}
